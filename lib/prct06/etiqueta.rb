6c3f2b163f19ba3602c5b7db6e073207fe8c29dd
Fernando González <alu0101045870@ull.edu.es>
2018-11-08 13:22:38 +0000

    Pruebas de valor energetico PASADAS

--- lib/prct06/etiqueta.rb
+++ lib/prct06/etiqueta.rb
@@ -20,5 +20,52 @@ class Etiqueta
     @min = min
   end
 
+  def val_gras_kj
+    (@g * 37).truncate(2) 
+  end
+
+  def val_gras_kcal
+    (@g * 9).truncate(2)	  
+  end
+
+  def val_hidr_kj
+    (@hid * 17).truncate(2)	  
+  end
+
+  def val_hidr_kcal
+    (@hid * 4).truncate(2)	  
+  end
+
+  def val_fib_kj
+    (@fib * 8).truncate(2)
+  end
+
+  def val_fib_kcal
+    (@fib * 2).truncate(2)	  
+  end
+
+  def val_prot_kj
+    (@pr * 17).truncate(2)
+  end
+
+  def val_prot_kcal
+    (@pr * 4).truncate(2)
+  end
+
+  def val_sal_kj
+    (@sal * 25).truncate(2)	  
+  end
+
+  def val_sal_kcal
+    (@sal * 6).truncate(2)
+  end
+
+  def val_ene_kj
+    val_gras_kj + val_hidr_kj + val_fib_kj + val_prot_kj + val_sal_kj	  
+  end
+
+  def val_ene_kcal
+    val_gras_kcal + val_hidr_kcal + val_fib_kcal + val_prot_kcal + val_sal_kcal  
+  end
 
 end
