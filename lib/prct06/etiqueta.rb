9e94866d00bc0063d97ac8efde1eaa055cfe476f
Fernando González <alu0101045870@ull.edu.es>
2018-11-08 11:17:54 +0000

    Pruebas generales pasadas

--- lib/prct06/etiqueta.rb
+++ lib/prct06/etiqueta.rb
@@ -2,8 +2,18 @@ class Etiqueta
   attr_reader :name, :g, :sat, :msat, :psat, :hid, :a, :palc, :alm, :fib, :pr, :sal
 
   def initialize(n,g,s,ms,ps,h,a,pa,al,fib,p,sal)
-     @name = n    
-  
+    @name = n    
+    @g = g
+    @sat = s
+    @msat = ms
+    @psat = ps
+    @hid = h
+    @a = a
+    @palc = pa
+    @alm = al
+    @fib = fib
+    @pr = p
+    @sal = sal 
   end
 
 
