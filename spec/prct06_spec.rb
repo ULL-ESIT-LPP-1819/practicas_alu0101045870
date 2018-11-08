ceb32b1b3334571ea023fdcec41146895af4a062
Fernando González <alu0101045870@ull.edu.es>
2018-11-08 11:09:22 +0000

    Prueba general de atributos

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -11,8 +11,53 @@ RSpec.describe Etiqueta do
   end
 
   describe "# inicializacion de atributos" do
+    
     it "Se almacena correctamente el nombre del articulo" do
       expect(@et1.name).to eq "Cacaolat"
+    end
+
+    it "Se almacena correctamente el valor de grasas" do
+      expect(@et1.g).to eq(0.5)
+    end
+
+    it "Se almacena correctamente el valor de las grasas saturadas" do
+      expect(@et1.sat).to eq(0.3)
+    end
+
+    it "Se almacena correctamente el valor de las grasas monosaturadas" do
+      expect(@et1.msat).to eq(0.2)
+    end
+
+    it "Se almacena correctamente el valor de las grasas polisaturadas" do
+      expect(@et1.psat).to eq(0.1)
+    end
+
+    it "Se almacena correctamente el valor de los hidratos de carbono" do
+      expect(@et1.hid).to eq(4.3)
+    end
+
+    it "Se almacena correctamente el valor de los hidratos de los cuales son azucares" do
+      expect(@et1.a).to eq(4.1)
+    end
+
+    it "Se almacena correctamente el valor de los polialcoholes" do
+      expect(@et1.palc).to eq(0)
+    end
+
+    it "Se almacena correctamente el valor de los almidones" do
+      expect(@et1.alm).to eq(0.2)
+    end
+    
+    it "Se almacena correctamente el valor de la fibra alimentaria" do
+      expect(@et1.fib).to eq(3)
+    end
+
+    it "Se almacena correctamente el valor de las proteinas" do
+      expect(@et1.pr).to eq(2.7)
+    end
+
+    it "Se almacena correctamente el valor de las sales" do
+      expect(@et1.sal).to eq(0.11)
     end
   end
 
