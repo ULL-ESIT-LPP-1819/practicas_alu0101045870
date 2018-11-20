6d8715265dc966b0c74c79e3d6c3e7062c84a45c
Fernando González <alu0101045870@ull.edu.es>
2018-11-20 17:08:03 +0000

    Pruebas para tipos de objetos

--- spec/prct08_spec.rb
+++ spec/prct08_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe Paciente do
 
 	before :all do
-		@p1 = Paciente.new("Fernando", "González", "Hombre", 20, 72, 1.78, 82, 92)
+		@list = Lista.new
+		@p1 = Paciente.new("Fernando", "González", 1, 20, 72, 1.78, 82, 92)
+		@p2 = Paciente.new("Jorge", "González", 1, 20, 104, 1.85, 80, 91)
+		@p3 = Paciente.new("Miriam","Rodríguez", 0, 24, 52, 1.57, 64, 86)
+		@p4 = Paciente.new("Kevin", "García", 1, 20, 90, 1.78, 102, 107)
+		@p5 = Paciente.new("Rey", "Misterio", 0, 43, 92, 1.65, 85, 92) 	
 	end
 	
 	describe "#Pruebas de inicializacion: " do
@@ -10,7 +15,7 @@ RSpec.describe Paciente do
 				expect(@p1.name).to eq("Fernando")
 				expect(@p1.surn).to eq("González")
 				expect(@p1.age).to eq(20)
-				expect(@p1.gen).to eq("Hombre")
+				expect(@p1.gen).to eq(1)
 			end
 		end
 		context "Clase hija: " do
@@ -23,5 +28,31 @@ RSpec.describe Paciente do
 		end
 	end
 
-	
+	describe "#Pruebas de herencia y tipado: " do
+		context "Tipo de objeto: " do
+		   it "Paciente:" do
+			expect(@p1.respond_to?('imc')).to be true
+			expect(@p1.respond_to?('to_s')).to be true
+		   end
+		   it "Lista:" do
+		   	expect(@list.respond_to?('extract_head')).to be true
+			expect(@list.respond_to?('insert_tail')).to be true
+		   end
+		   it "Individuo:" do
+		        @i1 = Individuo.new("Fernando", "Gonzalez", 1, 20)
+			expect(@i1.respond_to?('imc')).to be false
+			expect(@i1.respond_to?('to_s')).to be true
+		   end
+		end
+		it "Clase de objeto: " do
+		   expect(@p1.class == Paciente).to be true
+		   expect(@p1.instance_of?Paciente).to be true
+		end
+		it "Pertenece a la jerarquía: " do
+		end
+	end
+
+	describe "#Pruebas de clasificación en lista: " do
+		
+	end	
 end
