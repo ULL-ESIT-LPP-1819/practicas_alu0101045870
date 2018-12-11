8556c1bf647e3ddadd9d727f15ad16fd8d125fe0
Fernando González <alu0101045870@ull.edu.es>
2018-12-11 16:15:18 +0000

    Prueba para recolección de valor energético de menú

--- spec/prct10_spec.rb
+++ spec/prct10_spec.rb
@@ -39,8 +39,24 @@ RSpec.describe Lista do
     		@et3 = Etiqueta.new("Colacao",  220, 1, 3.7, 2.5, 1.7, 0.8, 20.1, 19.6, 0.9, 3, 1, 7.2, 0.17, 0.13, 0.13)
     		@et4 = Etiqueta.new("Caocola",  225, 1, 3.5, 2.2, 1.5, 0.7, 22.0, 21.1, 0.8, 3, 1, 7.3, 0.18, 0.9, 0.13)  
 
+		@lpac.insert_head(@p1)
+		@lpac.insert_head(@p2)
+		@lpac.insert_head(@p3)
+		@lpac.insert_head(@p4)
+		@lpac.insert_head(@p5)
+
+		@menu.insert_tail(@et1)
+		@menu.insert_tail(@et2)
+		@menu.insert_tail(@et3)
+		@menu.insert_tail(@et4)
+
 	end
 
-	
+	describe " # Recuperar total de valor energético del menú: " do
+		it "Sumatorio de elementos del menú" do
+			expect(@menu.reduce(0){|sum, num| (sum + num.val_ene_kcal).round(2) }).to eq(489.16)		
+		end
 
+			
+	end
 end
